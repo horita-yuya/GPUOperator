e5a106b2cdc092df93e20fbbea5c80a2f95b9fee
Yuya Horita <yuya@horita-yuya.local>
2019-04-20 04:00:05 +0900

    Update

--- Example/Example/Fourier/Fourier.swift
+++ Example/Example/Fourier/Fourier.swift
@@ -7,7 +7,7 @@ final class Fourier: Kernel {
     private var time: Float = 0;
     func encode(withEncoder encoder: MTLComputeCommandEncoder) {
         encoder.setBytes(&time, length: MemoryLayout<Float>.size, index: 0)
-        time += 0.25
+        time += 0.55
         
         if time > 100 { time = 0 }
     }
